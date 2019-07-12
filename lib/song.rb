771bdb3a6427e282e69a3adbd64ba4ed88f4a263
Paul Holmes <paulholmes1993@gmail.com>
2019-07-12 23:37:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ def self.new_from_filename(name)
 end
 
 def self.create_from_filename(name)
-  song = self.new 
+  song = self.new
   song.name = (name.split(" - ")[1].chomp(".mp3"))
   song.artist_name = (name.split(" - ")[0])
   @@all << song
