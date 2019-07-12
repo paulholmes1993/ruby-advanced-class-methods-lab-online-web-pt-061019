2a4dbe0d1359ca17f87f83cfa7e2f142c358fb91
Paul Holmes <paulholmes1993@gmail.com>
2019-07-12 23:29:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,10 @@ end
 
 def self.find_by_name(name)
   @@all.find{|x| x.name == name}
+end
 
-def self.find_or_create_by_name(name) || self.create_by_name(name)
+def self.find_or_create_by_name(name)
+  self.find_by_name(name)|| self.create_by_name(name)
 end
 
 end
